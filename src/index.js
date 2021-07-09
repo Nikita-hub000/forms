a1a31703d68bab27879fe332b8a8836f1cf24823
Nikita Lapshin <nikita.lapshin2000@gmail.com>
2021-07-10 01:19:36 +0600

    finished card adding

--- src/index.js
+++ src/index.js
@@ -27,17 +27,75 @@ class Price {
 
     this.el.querySelector(".radio-form__text").style.color = "#23C967";
   }
-  continue() {}
+  continue() {
+
+    function render(i) {
+      const root = document.querySelectorAll(".main-form__root")[0];
+      const product = document.createElement("p");
+      product.classList.add("main-form__title");
+      product.textContent = `Product ${i+1}`;
+      const keyword = document.createElement("p");
+      keyword.classList.add("main-form__text");
+      keyword.textContent = "Enter main keyword for the product";
+      const example = document.createElement("input");
+      example.classList.add("main-form__input");
+      example.placeholder = "for example, sylicon wine cup";
+      example.type = "text";
+      const link = document.createElement("p");
+      link.classList.add("main-form__text");
+      link.textContent = "Enter link to the similar product as a reference";
+      const http = document.createElement("input");
+      http.placeholder = "https://...";
+      http.type = "text";
+      http.classList.add("main-form__input");
+      root.appendChild(product);
+      root.appendChild(keyword);
+      root.appendChild(example);
+      root.appendChild(link);
+      root.appendChild(http);
+    }
+
+    let arr = Array.from(
+      this.el.parentNode.querySelectorAll(".radio-form__radio")
+    );
+    arr.forEach((x, index) => {
+      console.log(x.checked);
+
+      if (x.checked && index == 0) {
+        for (let i = 0; i < 5; i++) {
+          render(i)
+        }
+      }
+      else if(x.checked && index == 1){
+        for (let i = 0; i < 4; i++) {
+          render(i)
+        }
+      }
+      else if(x.checked && index == 2){
+        for (let i = 0; i < 3; i++) {
+          render(i)
+        }
+      }
+      else if(x.checked && index == 3){
+        for (let i = 0; i < 2; i++) {
+          render(i)
+        }
+      }
+      else if(x.checked && index == 4){
+          render(1)
+      }
+    });
+  }
 }
 
 class Transition {
-  constructor(el, f, currentf) {
+  constructor(el, f, currentf, preloader) {
     this.el = el;
     this.f = f;
     this.currentf = currentf;
+    this.preloader = preloader;
   }
   move() {
-    console.log(this.el);
     this.el.addEventListener("click", (e) => {
       e.preventDefault();
       this.currentf.classList.add("hide");
@@ -45,9 +103,47 @@ class Transition {
       this.f.classList.add("anim");
     });
   }
+  submit() {
+    this.el.addEventListener("click", (e) => {
+      e.preventDefault();
+      let a = this.el.textContent;
+      this.el.textContent = "";
+      this.preloader.style.display = "flex";
+
+      setTimeout(() => {
+        this.currentf.classList.add("hide");
+        this.f.classList.remove("hide");
+        this.f.classList.add("anim");
+        this.el.textContent = a;
+        this.preloader.style.display = "none";
+      }, 1000);
+    });
+  }
 }
 const button = document.querySelector(".main-form__button");
+const buttonSub = document.querySelectorAll(".main-form__submit")[0];
+const buttonBack = document.querySelectorAll(".main-form__submit")[2];
+const buttonNext = document.querySelectorAll(".main-form__submit")[1];
+const buttonTryAgain = document.querySelector(".main-form__submit_red");
+const preloader = document.querySelector(".preloader");
 const form1 = document.querySelectorAll(".main-form")[1];
 const form2 = document.querySelectorAll(".main-form")[0];
+const formBack = document.querySelectorAll(".main-form")[2];
+const formTryAgain = document.querySelectorAll(".main-form")[3];
+const formNext = document.querySelectorAll(".main-form")[1];
+
 const transition = new Transition(button, form1, form2);
+const submit = new Transition(
+  buttonSub,
+  Math.random() >= 0.5 ? formBack : formTryAgain,
+  form2,
+  preloader
+);
+const back = new Transition(buttonBack, form2, formBack);
+const tryAgain = new Transition(buttonTryAgain, form2, formTryAgain);
+const next = new Transition(buttonNext, form2, formNext);
 transition.move();
+submit.submit();
+back.move();
+tryAgain.move();
+next.move();
